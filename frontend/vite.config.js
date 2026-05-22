2548dfeaf688bba6b57d2638b5fdd709acdcdd61
Pooja Sukhdeve <poojasukhdeve06@gmail.com>
2026-05-22 19:58:33 +0530

    fix github pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/stock-momentum-analyzer/',
-  server: { port: 5173 }
+  base: '/stock_momentum_analyzer/',
 });
